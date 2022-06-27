1e3e39e47df43f75111150468939848e09611ee2
Marc <marc@PCFMB.IFABE>
2022-06-27 20:44:44 +0200

    Neue Versionen Mo 27 Jun 2022 20:44:44 CEST

--- backup_scripts.sh
+++ backup_scripts.sh
@@ -10,8 +10,8 @@ echo Push nach GitHub
 cd ~/Aufgaben
 git status
 git add .
-#git push https://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@github.com/marcflueckiger/BASH_Kurs.git
-git push https://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@github.com/marcflueckiger/BASH_Kurs.git 
+git push https://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@github.com/marcflueckiger/BASH_Kurs.git
+#git push https://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@github.com/marcflueckiger/BASH_Kurs.git 
 git commit -m "Neue Versionen $(date)"
 git status
 
