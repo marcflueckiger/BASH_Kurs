d9954604bdfb209d1068c73fb742249fbea9d9b8
marc.flueckiger <marc.flueckiger@students.ipso.ch>
2022-08-21 08:29:24 +0200

    Neue Versionen So 21 Aug 2022 08:29:24 CEST

--- backup_scripts.sh
+++ backup_scripts.sh
@@ -22,7 +22,7 @@ git status
 git add . 
 
 #newest
-git push https://marcflueckiger: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@github.com/marcflueckiger/BASH_Kurs.git
+git push https://marcflueckiger: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@github.com/marcflueckiger/BASH_Kurs.git
 #git push https://marcflueckiger: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@github.com/marcflueckiger/BASH_Kurs.git
 # git push https://github.com/marcflueckiger/BASH_Kurs.git
 
